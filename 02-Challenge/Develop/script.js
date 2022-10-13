9c9802d948cf9b1aeea87dfd7550f623a53fc7ca
mintedd <xxxxiheartyouxxxxx@gmail.com>
2022-10-13 15:48:08 -0400

    fixed first for loop options.length was supposed to be options.userInput and it works fine

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -1,39 +1,40 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var uppercase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var special = ["!", "@", "#", "$", "%", "^", "&", "*", "~"];
 var numberInt = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,];
 
 //Step 1: defining passwordCriteria because it is being called in the generatePassword function declaration 
 function passwordCriterias() {
     var userInput = parseInt(prompt("How many characters would you like your password to be"))
-        if (isNaN(userInput) || userInput < 8 || userInput > 128) {
-            window.alert("Password length has to be a number, and 8-128 characters. Please try again");
-            return null;
-        }//if the value entered is not a number or less than 8 or less than 128 null - try again 
+    if (isNaN(userInput) || userInput < 8 || userInput > 128) {
+        window.alert("Password length has to be a number, and 8-128 characters. Please try again");
+        return null;
+    }//if the value entered is not a number or less than 8 or less than 128 null - try again 
 
-        var hasLowercase = confirm("Would you like lowercase letters?")
-        var hasUppercase = confirm("Would you like uppercase letters?")
-        var hasSpecial = confirm("Would you like special characters?")
-        var hasNumberInt = confirm("Would you like numbers?")
+    var hasLowercase = confirm("Would you like lowercase letters?")
+    var hasUppercase = confirm("Would you like uppercase letters?")
+    var hasSpecial = confirm("Would you like special characters?")
+    var hasNumberInt = confirm("Would you like numbers?")
 
-        if (hasLowercase === false && hasUppercase === false && hasSpecial === false && hasNumberInt === false) {
-            window.alert("Must pick at least 1 criteria");
-            return null;
-        } //if user enters no for all of the criterias - null must try again because they must select at least one
+    if (hasLowercase === false && hasUppercase === false && hasSpecial === false && hasNumberInt === false) {
+        window.alert("Must pick at least 1 criteria");
+        return null;
+    } //if user enters no for all of the criterias - null must try again because they must select at least one
 
-        //still confused on whats going on here
-        var passwordOptions = {
-            userInput: userInput,
-            hasLowercase: hasLowercase,
-            hasUppercase: hasUppercase,
-            hasSpecial: hasSpecial,
-            hasNumberInt: hasNumberInt,
-        }
-        return passwordOptions;
+    //this will take what the user chose was for the criteria and will take that boolean tue or false
+    var passwordOptions = {
+        userInput: userInput,
+        hasLowercase: hasLowercase,
+        hasUppercase: hasUppercase,
+        hasSpecial: hasSpecial,
+        hasNumberInt: hasNumberInt,
+    }
+    return passwordOptions;
 }
+
 function randomizer(arr) { //arr is just a placeholder, 
     var randomIndex = Math.floor(Math.random() * arr.length)
     var randomElement = arr[randomIndex]
@@ -68,16 +69,15 @@ function generatePassword() {
     }
 
     //create a for loop options.length
-    for (let i = 0; i < options.length; i++) {
+    for (let i = 0; i < options.userInput; i++) {
         var possibleChar = randomizer(possibleArr)
         resultArr.push(possibleChar)
     }
 
     for (let i = 0; i < choiceArr.length; i++) {
-        resultArr[i]=choiceArr[i]
-        return resultArr.join("")
-
+        resultArr[i] = choiceArr[i]
     }
+    return resultArr.join("")
     // for some reason it is only giving me on 1 string value...
 }
 
@@ -85,9 +85,7 @@ function generatePassword() {
 function writePassword() {
     var password = generatePassword();
     var passwordText = document.querySelector("#password"); //here is where the random password will be shown
-
     passwordText.value = password; //.value will return the array
-
 }
 
 // Add event listener to generate button
